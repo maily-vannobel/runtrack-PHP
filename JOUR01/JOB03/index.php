5391276189fe763fee6840313114988bb36e1eaa
maily-vannobel <maily.vannobel@laplateforme.io>
2024-06-03 14:11:59 +0200

    job3 ok (J1)

--- JOUR01/JOB03/index.php
+++ JOUR01/JOB03/index.php
@@ -1,4 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>J1-JOB3</title>
+</head>
+<body>
+
 <?php
-
-
+$intVar = 13;
+$floatVar = 4.2;
+$stringVar = "Maïly";
+$boolVar = true;
 ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Entier</td>
+            <td>intVar</td>
+            <td><?= $intVar ?></td>
+        </tr>
+        <tr>
+            <td>Float</td>
+            <td>floatVar</td>
+            <td><?= $floatVar ?></td>
+        </tr>
+        <tr>
+            <td>String</td>
+            <td>stringVar</td>
+            <td><?= $stringVar ?></td>
+        </tr>
+        <tr>
+            <td>Boolean</td>
+            <td>boolVar</td>
+            <td><?= $boolVar ? 'true' : 'false' ?></td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
