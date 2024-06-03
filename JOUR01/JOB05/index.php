6314f5a34de8c3002aae9f3b375ff26f80822eb8
maily-vannobel <maily.vannobel@laplateforme.io>
2024-06-03 14:43:00 +0200

    job5 ok (job1)

--- JOUR01/JOB05/index.php
+++ JOUR01/JOB05/index.php
@@ -2,9 +2,9 @@
 
 // i initalisé à 0 -> boucle continu d'incrémenter tps que i inf/= à 1337
 for ($i = 0; $i <= 1337; $i++) {
-// si $i=42, appliquer style
-if ($i == 42) {
-    echo "<b><u>$i</b></u><br>";
+// si $i=26,37,88 ou111 applique des styles spécifiques
+if ($i == 26 || $i == 37 || $i == 88 || $i == 1111) {
+    echo null;
 } else {
     // sinn affiche justre valeur de $i
     echo $i . "<br>";
